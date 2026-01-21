5cbe7a66e5e932d6e6df1437e59b80061d1b03d0
bijaykumarpun <bijay.workplace@gmail.com>
2026-01-21 18:13:52 +0545

    fix(firebase-remote-config): add missing initialization code

--- app/src/main/java/rs/android/launchpad/LaunchpadApplication.kt
+++ app/src/main/java/rs/android/launchpad/LaunchpadApplication.kt
@@ -2,7 +2,12 @@ package rs.android.launchpad
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import rs.android.launchpad.util.remote_config.FirebaseRemoteConfiguration
 
 @HiltAndroidApp
 class LaunchpadApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseRemoteConfiguration().initialize()
+    }
 }
