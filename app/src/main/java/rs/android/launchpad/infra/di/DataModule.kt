c09b5b10f2e96685ba237a89b7fb4911c842d62d
bijaykumarpun <bijay.workplace@gmail.com>
2026-01-29 04:27:20 +0545

    fix(billingmanager): add missing injection module for the class

--- app/src/main/java/rs/android/launchpad/infra/di/DataModule.kt
+++ app/src/main/java/rs/android/launchpad/infra/di/DataModule.kt
@@ -1,10 +1,12 @@
 package rs.android.launchpad.infra.di
 
+import android.content.Context
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import rs.android.launchpad.repository.FeatureRepository
 import rs.android.launchpad.repository.ProductRepository
@@ -12,6 +14,7 @@ import rs.android.launchpad.repository.RemoteConfigRepository
 import rs.android.launchpad.repository.impl.FeatureRepositoryImpl
 import rs.android.launchpad.repository.impl.ProductRepositoryImpl
 import rs.android.launchpad.repository.impl.RemoteConfigRepositoryImpl
+import rs.android.launchpad.util.billing.BillingManager
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -34,6 +37,19 @@ abstract interface DataModule {
     ): ProductRepository
 
 }
+
+@Module
+@InstallIn(SingletonComponent::class)
+object BillingModule {
+
+    @Provides
+    fun provideBillingManager(
+        @ApplicationContext context: Context
+    ): BillingManager {
+        return BillingManager(context)
+    }
+}
+
 
 @Module
 @InstallIn(SingletonComponent::class)
